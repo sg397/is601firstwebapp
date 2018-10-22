5e7f9247b1dba287ba962c62d02e4cbf96f3578a
sunitha <sg397@njit.edu>
2018-10-22 14:17:07 -0400

    Task: Test heroku auto deployment from github repo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HOME
                 </div>
 
                 <div class="links">
